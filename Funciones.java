2866a975d6755df8d74c9ec7d14ffedf886aee6b
francoC1410 <111617652+francoC1410@users.noreply.github.com>
2022-09-02 19:15:24 -0300

    Add files via upload

---
+++ Funciones.java
@@ -0,0 +1,17 @@
+package Tiposdedatos;
+
+public class Funciones {
+
+    public static void main(String[] args) {
+
+        double suma = precio1 + 17.25;
+
+        System.out.println(suma);
+    }
+
+    public static double precio1 = 242.55;
+
+}
+
+
+
